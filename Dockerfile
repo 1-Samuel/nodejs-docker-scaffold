d2838dc3d75f021577dbc20109c9ef6d486568d1
1-Samuel <1-samuel@not-trustworthy.link>
2023-02-23 15:07:30 +0100

    Added configuration

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:19
 
 WORKDIR /usr/src/app
+VOLUME [ "/usr/src/app/config" ]
 
 COPY package*.json ./
 
--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const express = require("express");
+import express, {json, urlencoded} from "express";
+import config from "config";
 
 const app = express();
 
 const port = 8080;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const response = config.get("response-string");
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
 app.get("/", (req, res) => {
-    res.status(200).json({ "get": "rolled" });
+    res.status(200).json({ "get": response });
 });
+
+app.get("/reload", (req, res) => {
+    delete require.cache[require.resolve('config')];
+})
